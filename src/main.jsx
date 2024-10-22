4340fd61cb8d0b84f378bbf7b9b14323850abd5e
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-10-22 13:42:44 +0300

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import * as Sentry from "@sentry/react";
+// WITH SENTRY
+// import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: "https://a0e425eb7b74ba5d40e1160a800f4c65@o4507419779530752.ingest.us.sentry.io/4508165761925120",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0, 
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0, 
-});
+// Sentry.init({
+//   dsn: "https://a0e425eb7b74ba5d40e1160a800f4c65@o4507419779530752.ingest.us.sentry.io/4508165761925120",
+//   integrations: [
+//     Sentry.browserTracingIntegration(),
+//     Sentry.reactRouterV6BrowserTracingIntegration({
+//       useEffect: React.useEffect,
+//     }),
+//     Sentry.replayIntegration({
+//       maskAllText: false,
+//       blockAllMedia: false,
+//     }),
+//   ],
+//   tracesSampleRate: 1.0, 
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0, 
+// });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
